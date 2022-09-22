2305ceabd06e140464b040558b6de05bae9fd412
miranda lin <miranda.lin@online-pajak.com>
2022-09-22 23:32:19 +0200

    added test for strike + refactoring

--- src/com/mir/bowlingKata/Game.java
+++ src/com/mir/bowlingKata/Game.java
@@ -13,14 +13,35 @@ public class Game {
         int frameNumber = 0;
 
         for (int frame = 0; frame < 10; frame++) {
-            if (isSpare(frameNumber)) {
-                score += 10 + rolls[frameNumber+2];
-            } else {
-                score += rolls[frameNumber] + rolls[frameNumber + 1];
+            if (isStrike(frameNumber)) {
+                score += 10 + strikeBonus(frameNumber);
+                frameNumber++;
             }
-            frameNumber += 2;
+            else if (isSpare(frameNumber)) {
+                score += 10 + spareBonus(frameNumber);
+                frameNumber += 2;
+            } else {
+                score += sumOfPinsInFrame(frameNumber);
+                frameNumber += 2;
+            }
         }
         return score;
+    }
+
+    private int sumOfPinsInFrame(int frameNumber) {
+        return rolls[frameNumber] + rolls[frameNumber + 1];
+    }
+
+    private int spareBonus(int frameNumber) {
+        return rolls[frameNumber+2];
+    }
+
+    private int strikeBonus(int frameNumber) {
+        return rolls[frameNumber + 1] + rolls[frameNumber + 2];
+    }
+
+    private boolean isStrike(int frameNumber) {
+        return rolls[frameNumber] == 10;
     }
 
     private boolean isSpare(int frameNumber) {
--- test/com/mir/bowlingKata/GameTest.java
+++ test/com/mir/bowlingKata/GameTest.java
@@ -55,6 +55,25 @@ class GameTest {
         assertEquals(16, game.score());
     }
 
+    @Test
+    public void gameWith1Strike_ShouldCalculateStrikeBonus() {
+        // given
+        setup();
+
+        // when
+        rollStrike();
+        game.roll(3);
+        game.roll(5);
+        playGame(16, 0);
+
+        // then
+        assertEquals(26, game.score());
+    }
+
+    private void rollStrike() {
+        game.roll(10);
+    }
+
     private void rollSpare() {
         game.roll(5);
         game.roll(5);
